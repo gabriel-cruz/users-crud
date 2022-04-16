82e40b265e403862c1844c3eb21c6f493e2ed3dc
Gabriel Cruz <gabrielcruz7400@gmail.com>
2022-04-16 10:14:28 -0300

    make dynamic dropdown for state and cities

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 use Redirect;
 use App\Models\User;
+use App\Models\State;
+use App\Models\City;
 
 
 class UserController extends Controller
 {
-    public $state;
+    public function __construct(State $state, City $city){
+        $this->state = $state;
+        $this->city = $city;
+    }
 
     public function index(){
         $users = User::get();
@@ -18,31 +23,26 @@ class UserController extends Controller
     }
 
 
-    public function create(){
-        $states = json_decode(
-            Http::get("https://servicodados.ibge.gov.br/api/v1/localidades/estados", ['OrderBy' => 'nome'])->body()
-        );
-
-        $cities = [];
-
-        if($this->state){
-            $cities = json_decode(
-                HTTP::get(
-                    "https://servicodados.ibge.gov.br/api/v1/localidades/estados/{$this->state}/municipios")->body()
-                );
-        }
+    public function create(){       
+        $states = $this->state->orderBy('name', 'ASC')->get();
+        //$cities = $this->city->Where('id', '=', 0)->orderBy('name', 'ASC')->get();
         
-        
-        return view('users.form' , 
-        ['states' => $states,
-          'cities' => $cities]);
+        return view('users.form', ['states' => $states]);
     }
 
-    public function add(Request $request){
-        $user = new User;
-        $user = $user->create($request->all());
+    public function loadCities(Request $request){
+        echo $stateId = $request->post('stateId');
+        $city = DB::table('cities')->where('state_id', $stateId)->orderBy('name', 'ASC')->get();
+        
+        $html = '<option value="">Selecione a Cidade</option>';
+        foreach ($city as $list){
+            $html.='<option value="'.$list->id.'">'.$list->name.'</option>';
+        }
 
-        return Redirect::to('/users');
+        // $dataForm = $request->all();
+        // $state_id = $dataForm['state_id'];
 
+        
+        // return $cities;
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css');
